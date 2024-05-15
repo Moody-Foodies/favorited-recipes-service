93b0168a4da37ea2fa0ddc6b6d964b1f206862b0
Jack Sweeney <jackcsweeney@gmail.com>
2024-05-15 16:27:04 -0700

    feat: Update factory bot to create FavoriteRecipe objects correctly

--- spec/factories/favorite_recipes.rb
+++ spec/factories/favorite_recipes.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :favorite_recipe do
-    id {Faker::Number.numer}
+    id {Faker::Number.number(digits: 5)}
     user_id {1}
     name {Faker::Food.dish}
     description {Faker::Food.description}
-    time_to_cook {Faker::Number.number}
+    time_to_cook {Faker::Number.number(digits: 5)}
     nutrient {Faker::Food.ingredient}
     health_benefits {Faker::Adjective.positive}
     image {"food.jpeg"}
-    ingredients {[Faker::Food.ingredient, Faker::Food.ingredient]}
-    instructions {[Faker::Food.description, Faker::Food.description]}
+    ingredients {[Faker::Food.ingredient, Faker::Food.ingredient].join("*separator*")}
+    instructions {[Faker::Food.description, Faker::Food.description].join("*separator*")}
   end
 end
